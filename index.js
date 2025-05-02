d60afaab12dee12c4a014a9026cedbfe1a5083be
MeGustav0 <leha-bolshakov02@mail.ru>
2025-05-02 11:40:38 +0300

    v3

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://usm-platform-d16ff.web.app"],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Роуты
